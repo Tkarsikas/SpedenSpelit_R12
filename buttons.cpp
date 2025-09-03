026adef1d0b04a6e4bca54f97edf7bb80518dfa2
Tkarsikas <tommi.karsi@gmail.com>
2025-09-03 16:28:11 +0300

    aliohjelmien tallennus gittiin

--- buttons.cpp
+++ buttons.cpp
@@ -6,6 +6,12 @@
 void initButtonsAndButtonInterrupts(void)
 {
   // See requirements for this function from buttons.h
+  int button[] = {2,3,4,5,6};
+  for(int i =0; i<5; i++){
+    pinMode(button[i], INPUT_PULLUP); //pinnien ylösveto
+  }
+  PCICR |= 0b00000100; //enable pcmsk2 eli pinnit 0-7 voi tehdä keskeytyksen
+  PCMSK2 |= 0b01111100; //seuraa pinnejä 2-6
 }
 
 ISR(PCINT2_vect) {
@@ -14,5 +20,59 @@ ISR(PCINT2_vect) {
 	 interrupts from 2,3,4,5 pins for Game push buttons
 	 and for pin 6 for start Game push button.
    */
-}
+   static byte portDMSK = 0b01111100; //0b01111100 seuraa D-portin pinnien statusta jatkuvasti
+  
+   /*Kun joku nappi painetaan niin PIND tavussa muuttuu nappia vastaava bitti.
+   xor-operaatiolla verrataan lähtötilannetta napin painallus hetkeen. jos muutosta ei tapahdu xor operaation tulos = b00000000 */
+   byte currentState = PIND;
+   byte changedBit = portDMSK ^ currentState; 
+   int nappi=-1;
+   //if(!currentState & portDMSK){
+    for(int i =2; i<=6; i++){
+      if(bitRead(changedBit, i) && (!bitRead(currentState, i))){
+            Serial.print("nappi ");
+            nappi +=i;
+            Serial.print(nappi);
+            Serial.println(". painettu");
+      }
+    }
+   //}
+   
+   /*if(changedBit & 0b00000100 ){ // jos 1. nappi painettu changedBit=0b00000100 -> 0b00000100 and 0b00000100 = 1 eli true
+     if(!(PIND & 0b00000100 )){
+      //delay(30);
+       // Serial.print(changedBit);
+        Serial.println("1. nappi painettu");
+        delay(30);
+      }
+    }
+    if(changedBit & 0b00001000 ){ // jos 2. nappi painettu changedBit=0b00001000 -> 0b00001000 and 0b00001000 = 1 eli true
+     if(!(PIND & 0b00001000 )){
+      delay(30);
+             Serial.println("2. nappi painettu");
 
+      }
+    }
+    if(changedBit & 0b00010000 ){ // jos 3. nappi painettu changedBit=0b00010000 -> 0b00010000 and 0b00010000 = 1 eli true
+     if(!(PIND & 0b00010000 )){
+      delay(30);
+      Serial.println("3. nappi painettu");
+
+      }
+    }
+    if(changedBit & 0b00100000 ){ // jos 4. nappi painettu changedBit=0b00100000 -> 0b00100000 and 0b00100000 = 1 eli true
+     if(!(PIND & 0b00100000 )){
+      delay(30);
+           Serial.println("4. nappi painettu");
+
+      }
+    }
+    if(changedBit & 0b01000000 ){ // jos 5. nappi painettu changedBit=0b01000000 -> 0b01000000 and 0b01000000 = 1 eli true
+     if(!(PIND & 0b01000000 )){
+      delay(30);
+     Serial.println("5. nappi painettu");
+
+      }
+    }*/
+    
+}
--- leds.cpp
+++ leds.cpp
@@ -48,9 +48,36 @@ for(int i = 0; i<4; i++){
 void show1()
 {
 // see requirements for this function from leds.h
+int analogPin[] = {A2,A3,A4,A5};
+
+for (int value =0; value < 16; value++){
+  
+  for(int led =0; led<4; led++){
+    digitalWrite(analogPin[led], (value >> led) & 0x01);
+    //delay(50);
+  }
+  delay(500);
+}
 }
 
 void show2(int rounds)
 {
-// see requirements for this function from leds.h  
+// see requirements for this function from leds.h 
+// see requirements for this function from leds.h 
+int analogPin[] = {A2,A3,A4,A5};
+int minDelay =0;
+int maxDelay =500;
+int delayDecrease = (maxDelay/rounds)+50;
+
+while(maxDelay>minDelay){
+ for(int led=0;led<4;led++){
+  digitalWrite(analogPin[led], HIGH);
+  delay(maxDelay);
+    digitalWrite(analogPin[led], LOW);
+    delay(maxDelay);
+
+}
+    maxDelay -=delayDecrease;
+
+ }
 }
