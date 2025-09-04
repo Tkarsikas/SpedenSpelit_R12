d3539720c078ebe978ef8c2e5819c3b538125b5d
Etunimesi Sukunimesi <shkpostisi@example.com>
2025-09-04 16:15:46 +0300

    ShiftOut -> For-loop muutos

--- display.cpp
+++ display.cpp
@@ -6,18 +6,30 @@ int outEnable = 9;
 int latchClock = 10;
 int shiftClock = 11;
 int displayReset = 12;
-
-byte numerotBitteina[] = { // Taulukko binäärisenä numeroista segmenttinäytöille, Globaali taulukko
-  0b00111111, // 0
-  0b00000110, // 1
-  0b01011011, // 2
-  0b01001111,
-  0b01100110,
-  0b01101101,
-  0b01111101,
-  0b00000111,
-  0b01111111,
-  0b01101111
+/*
+const byte numerotBitteina[10][8] = { // Segmenttien testaus, Globaali taulukko
+  {1,0,0,0,0,0,0,0}, // tyhjä (Lähtö Q7, ei ole kytkettynä. Voitaisi kytkeä pisteeseen näytön alareunassa)
+  {0,1,0,0,0,0,0,0}, // Keskiviiva
+  {0,0,1,0,0,0,0,0}, // Vasen ylempi
+  {0,0,0,1,0,0,0,0}, // Vasen alempi
+  {0,0,0,0,1,0,0,0}, // Alin viiva
+  {0,0,0,0,0,1,0,0}, // Oikea alhaalla
+  {0,0,0,0,0,0,1,0}, // Oikea ylhäällä
+  {0,0,0,0,0,0,0,1}, // Yläviiva
+  Syttyvä segmentti riippuu kytkennästä. Nykyisellä kytkennällä meni näin
+};
+*/
+const byte numerotBitteina[10][8] = { // Taulukko segmenttinäyttöojen ohjaukseen, Globaali taulukko
+  {0,0,1,1,1,1,1,1}, // 0
+  {0,0,0,0,0,1,1,0}, // 1
+  {0,1,0,1,1,0,1,1}, // 2
+  {0,1,0,0,1,1,1,1}, // 3
+  {0,1,1,0,0,1,1,0}, // 4
+  {0,1,1,0,1,1,0,1}, // 5
+  {0,1,1,1,1,1,0,1}, // 6
+  {0,0,0,0,0,1,1,1}, // 7
+  {0,1,1,1,1,1,1,1}, // 8
+  {0,1,1,0,1,1,1,1}, // 9
 };
 
 void initializeDisplay(void) { // See requirements for this function from display.h
@@ -36,8 +48,27 @@ digitalWrite(outEnable, LOW); // Outputit aktiiviseksi
 }
 
 void writeByte(uint8_t bits,bool last) {   // See requirements for this function from display.h
-  digitalWrite(latchClock, LOW); // "Vedetään" ensin "latchClock" alas, jotta syötetyt bittisarjat ei mene suoraan segmentti näytöille
-  shiftOut(serialInput, shiftClock, MSBFIRST, numerotBitteina[bits]); // Läheteään bitit segmenttinäytöille
+  int taulukonNumero = 0;
+  for (int i = 0; i < 8 ; i++) {
+    int low_High = numerotBitteina[bits][i];
+    /*
+    Serial.print(("Taulukon numero on "));
+    Serial.print(low_High);
+    Serial.print(" numeron ");
+    Serial.print(bits);
+    Serial.print(" kohdalta. Menossa taulukon indeksi ");
+    Serial.println(taulukonNumero);
+    delay(300);
+    */
+    if (low_High == 0) {
+      digitalWrite(serialInput, LOW);
+    } else {
+      digitalWrite(serialInput, HIGH);
+    }
+    digitalWrite(shiftClock, HIGH); // Siirrettään bitti/yhden segmentin ohjaus kontrollerille
+    digitalWrite(shiftClock, LOW);
+    taulukonNumero++;
+  }
   if (last == true) {
     digitalWrite(latchClock, HIGH); // "Aktivoidaan" uusi syöte kunnes molemmat ykköset ja kympit on syötetty
   }
@@ -45,6 +76,7 @@ void writeByte(uint8_t bits,bool last) {   // See requirements for this function
 
 
 void writeHighAndLowNumber(uint8_t tens,uint8_t ones) { // See requirements for this function from display.h
+  digitalWrite(latchClock, LOW); // "Vedetään" ensin "latchClock" alas, jotta syötetyt bittisarjat ei mene suoraan segmentti näytöille
   writeByte(tens,false); // Ensin syötetään kympit, totuusarvo "false" -> Numerot ei vielä päivity
   writeByte(ones,true); // Perään syötetään ykköset, totuusarvo "true" -> Numerot päivittyy
 }
