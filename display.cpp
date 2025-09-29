d91ad70d19b35f9502f5a135503e18a8af2267da
Aapo <95102574+Kartsoful@users.noreply.github.com>
2025-09-29 17:19:36 +0300

    Kommentoitu yksityiskohtaisemmin ohjelmaa

--- display.cpp
+++ display.cpp
@@ -36,6 +36,13 @@ digitalWrite(outEnable, LOW); // Outputit aktiiviseksi
 }
 
 void writeByte(uint8_t bits,bool last) {   // See requirements for this function from display.h
+  /*
+  Tehdään ensin kahdeksan numeron taulukko
+  Selvitetään onko numero alle vai yli 10
+  Luetaan luotuun taulukkoon segmenttinäytöille ohjaus
+  Riippuen oliko arvo alle vai yli 10, muutetaan indeksin arvo 0 ykköseksi
+  -> Sytytetään myös piste palamaan. Visioi pisteitä jos pisteet yli 100 tai 200
+  */
   byte numero[8];
   if (bits < 10) {
     for (int i = 0; i < 8 ; i++) {
@@ -49,8 +56,14 @@ void writeByte(uint8_t bits,bool last) {   // See requirements for this function
     }
   }
 
+  /*
+  Lähetetään numero segmenttinäytöille
+  Käydään läpi jokainen binääri millä ohjataan segmenttinäyttöjä
+  ja lähetetään ne yksitellen serial-to-parallel muuntimelle,
+  joka puolestaan ohjaa segmenttinäyttöjä
+  */
   for (int i = 0; i < 8 ; i++) {
-    int low_High = numero[i];
+    int low_High = numero[i];  
     if (low_High == 0) {
       digitalWrite(serialInput, LOW);
     } else {
@@ -74,6 +87,14 @@ void writeHighAndLowNumber(uint8_t tens,uint8_t ones) { // See requirements for 
 void showResult(byte number) { // See requirements for this function from display.h
   uint8_t ones = number % 10;  // Lasketaan tulevan arvon jakojäännös kymmenellä
   uint8_t tens = number / 10;  // Jaetaan tuleva arvo kymmenellä -> saadaan kymmenet
+
+  /* 
+  Jos pisteiden jakojäännös yli 20 -> pisteitä yli 200
+  Tällöin muutetaan ykkösten arvoa +10, ja vähennetään kymmenien jakojäännöksestä 10
+  Näin sadaan kympit ja ykköset välille 10-19, jolloin syttyy myös segmenttinäyttöjen piste palamaan.
+  Pisteillä indikoidaan, onko pisteet välillä 100-199 / 200-299
+  Jos yksi piste palaa -> 100-199. Jos kaksi pistettä -> 200-299
+  */
   if (tens >= 20) {
     ones += 10;
     tens -= 10;
